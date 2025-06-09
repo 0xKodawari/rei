b34e69c1124edbbbb310f6cdca6a2734a1789c67
0xKodawari <jonmichael17@gmail.com>
2025-06-09 15:22:09 -0500

    tracker skeleton

--- crates/tracker/src/lib.rs
+++ crates/tracker/src/lib.rs
@@ -1,14 +1,125 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use chrono::{DateTime, NaiveDate, Utc};
+use std::collections::HashMap;
+use std::time::Duration;
+
+// Todo
+// 1. How to handle date changes and clean up between potential calls
+
+#[derive(Debug, Default, PartialEq, Eq, Clone)]
+pub struct DailyImmersion {
+    // Start of the most recent interval of immersion
+    pub set: DateTime<Utc>,
+    // Total Immersion on the day
+    pub total: Duration,
+    // Is this already being tracked or not
+    pub active: bool,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Goal {
+    // Daily desired amount spent on the goal
+    pub daily: Duration,
+    // Amount spent within the current day and historical information
+    pub current: HashMap<NaiveDate, DailyImmersion>,
+    // Total amount spent on the goal since tracking
+    pub total: Duration,
+}
+
+impl Goal {
+    // If you stop multiple times in a row it will still count as if you are immersing
+    pub fn stop(&mut self) {
+        let date = Utc::now();
+        let day = date.date_naive();
+        self.current
+            .entry(day)
+            .and_modify(|time| {
+                if time.active {
+                    let delta = date - time.set;
+                    let dur =
+                        Duration::new(delta.num_seconds() as u64, delta.subsec_nanos() as u32);
+                    // Have something in place for if this panics with nanoseconds
+                    time.total += dur;
+                    self.total += dur;
+                    time.active = false;
+                }
+            })
+            .or_default();
+    }
+
+    pub fn start(&mut self) {
+        let date = Utc::now();
+        let day = date.date_naive();
+        self.current
+            .entry(day)
+            .and_modify(|time| {
+                if !time.active {
+                    time.set = date;
+                    time.active = true;
+                }
+            })
+            .or_insert(DailyImmersion {
+                set: date,
+                total: Duration::ZERO,
+                active: true,
+            });
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Immersion {
+    pub listening: Goal,
+    pub reading: Goal,
+}
+
+impl Immersion {
+    pub fn new() -> Immersion {
+        Immersion {
+            listening: Goal {
+                daily: Duration::ZERO,
+                current: HashMap::new(),
+                total: Duration::ZERO,
+            },
+            reading: Goal {
+                daily: Duration::ZERO,
+                current: HashMap::new(),
+                total: Duration::ZERO,
+            },
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use std::time::Duration;
+
+    use chrono::Utc;
+
+    use super::Immersion;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_track() {
+        let date = Utc::now().date_naive();
+        let mut immersion = Immersion::new();
+
+        immersion.listening.start();
+        let current = immersion.listening.current.get(&date);
+        assert!(current.is_some());
+        let current = current.unwrap();
+        assert!(current.active);
+
+        std::thread::sleep(Duration::new(1, 0));
+
+        let mut clone = immersion.clone();
+        clone.listening.start();
+        let after_sleep_current = clone.listening.current.get(&date).unwrap();
+        assert_eq!(current, after_sleep_current);
+
+        immersion.listening.stop();
+        let updated_current = immersion.listening.current.get(&date).unwrap();
+
+        assert!(!updated_current.active);
+        assert!(!updated_current.total.is_zero());
+        assert!(!immersion.listening.total.is_zero());
+        assert_ne!(updated_current, after_sleep_current);
     }
 }
